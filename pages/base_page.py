c3299dbf9cec4e7901660e56d417a345fd7b4cc1
PavelN1979 <pavel_informatik@vk.com>
2024-10-18 21:53:08 +0500

    added inheritance pros: example

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,7 +1,11 @@
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from .locators import LoginPageLocators
 import math 
-\
+
 class BasePage():
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
@@ -35,3 +39,28 @@ class BasePage():
             alert.accept()
         except NoAlertPresentException:
             print("No second alert presented")
+    
+    def is_not_element_present(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+        return False
+
+
+    def is_disappeared(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException).\
+                until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+
+        return True
+
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(*LoginPageLocators.LOGIN_LINK)
+        login_link.click()
+
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_LINK), "Login link is not presented"
--- test_product_page.py
+++ test_product_page.py
@@ -1,11 +1,11 @@
-from .pages.product_page import LoginPage
+from .pages.product_page import ProductPage
+from .pages.login_page import LoginPage
 import pytest
 
 
-link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-
 def test_guest_can_add_product_to_basket(browser):
-    page = LoginPage(browser, link)   
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = ProductPage(browser, link)   
     page.open()
     page.add_product_to_cart()
     page.solve_quiz_and_get_code() 
@@ -17,9 +17,50 @@ def test_guest_can_add_product_to_basket(browser):
 @pytest.mark.parametrize('links',['?promo=offer0','?promo=offer1','?promo=offer2','?promo=offer3','?promo=offer4',
              '?promo=offer5','?promo=offer6', pytest.param('?promo=offer7', marks=pytest.mark.xfail), '?promo=offer8','?promo=offer9'])
 def test_guest_add_product_to_cart(browser, links):
-    page = LoginPage(browser,  f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/{links}")   
+    page = ProductPage(browser,  f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/{links}")   
     page.open()
     page.add_product_to_cart()
     page.solve_quiz_and_get_code() 
     page.added_product_by_name_product() 
     page.cost_of_the_ordered_goods()
+
+
+
+class TestProduct():
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+    @pytest.mark.xfail
+    def test_guest_cant_see_success_message_after_adding_product_to_basket(self, browser):
+        page = ProductPage(browser, self.link)   
+        page.open()
+        page.add_product_to_cart()
+        page.should_not_be_success_message()
+
+    def test_guest_cant_see_success_message(self, browser):
+        page = ProductPage(browser, self.link)   
+        page.open()
+        page.should_not_be_success_message()
+
+    @pytest.mark.xfail
+    def test_message_disappeared_after_adding_product_to_basket(self, browser):
+        page = ProductPage(browser, self.link)   
+        page.open()
+        page.add_product_to_cart()
+        page.should_dissapear_of_success_message()
+
+class TestLogin():
+    def test_guest_should_see_login_link_on_product_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_be_login_link()
+
+
+
+    def test_guest_can_go_to_login_page_from_product_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+        page = ProductPage(browser, link) 
+        page.open() 
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
